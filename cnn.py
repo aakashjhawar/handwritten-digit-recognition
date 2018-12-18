8574377f3c5a7390f4c682c73d673f04b58aa9ac
Aakash Jhawar <aakash.16.jhawar@gmail.com>
2018-12-18 16:19:16 +0530

    Modifie testing part

--- cnn.py
+++ cnn.py
@@ -10,24 +10,30 @@ from keras.layers.convolutional import Conv2D
 from keras.layers.convolutional import MaxPooling2D
 from keras.utils import np_utils
 from keras import backend as K
+import matplotlib.pyplot as plt
+
 K.set_image_dim_ordering('th')
-# fix random seed for reproducibility
+
+# Fix random seed for reproducibility
 seed = 7
 numpy.random.seed(seed)
-# load data
+
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
-# reshape to be [samples][pixels][width][height]
+
+# Reshape to be samples*pixels*width*height
 X_train = X_train.reshape(X_train.shape[0], 1, 28, 28).astype('float32')
 X_test = X_test.reshape(X_test.shape[0], 1, 28, 28).astype('float32')
-# normalize inputs from 0-255 to 0-1
+
+# Normalize inputs from 0-255 to 0-1
 X_train = X_train / 255
 X_test = X_test / 255
-# one hot encode outputs
+
+# One Hot encode outputs
 y_train = np_utils.to_categorical(y_train)
 y_test = np_utils.to_categorical(y_test)
 num_classes = y_test.shape[1]
 
-# create model
+# Create model
 model = Sequential()
 model.add(Conv2D(32, (5, 5), input_shape=(1, 28, 28), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
@@ -38,48 +44,37 @@ model.add(Flatten())
 model.add(Dense(128, activation='relu'))
 model.add(Dense(64, activation='relu'))
 model.add(Dense(num_classes, activation='softmax'))
-# Compile model
-model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-# Fit the model
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=10, batch_size=200)
+
 # Final evaluation of the model
 scores = model.evaluate(X_test, y_test, verbose=0)
 print("Large CNN Error: %.2f%%" % (100-scores[1]*100))
 
-#  - - - - - - - TEST single - - - - - - - -
-a1 = X_test[1] # 1->'2';    
-#a2 = a1 / 255
-a2 = a1.reshape(1,1,28,28)
-model_pred = model.predict_classes(a2, verbose = 0)
-print('Prediction of loaded_model: {}'.format(model_pred[0]))
+#  - - - - - - - TEST single image - - - - - - - -
+
+image = (X_test[1]).reshape(1,1,28,28) # 1->'2';    
+model_pred = model.predict_classes(image, verbose = 0)
+print('Prediction of model: {}'.format(model_pred[0]))
 
 
-# - - - - - - TESTING - - - - - - - - - -
-import matplotlib.pyplot as plt
+# - - - - - - TESTING multiple image - - - - - - - - - -
 
-# grab some test images from the test data
 test_images = X_test[1:5]
-
-# reshape the test images to standard 28x28 format
 test_images = test_images.reshape(test_images.shape[0], 28, 28)
-print ("[INFO] test images shape - {}".format(test_images.shape))
+print ("Test images shape: {}".format(test_images.shape))
 
-# loop over each of the test images
 for i, test_image in enumerate(test_images, start=1):
-	# grab a copy of test image for viewing
-	org_image = test_image
-	
-	# reshape the test image to [1x784] format so that our model understands
-	test_image = test_image.reshape(1,1,28,28)
-	
-	# make prediction on test image using our trained model
-	prediction = model.predict_classes(test_image, verbose=0)
-	
-	# display the prediction and image
-	print ("[INFO] I think the digit is - {}".format(prediction[0]))
-	plt.subplot(220+i)
-	plt.imshow(org_image, cmap=plt.get_cmap('gray'))
+    org_image = test_image
+    test_image = test_image.reshape(1,1,28,28)
+    prediction = model.predict_classes(test_image, verbose=0)
+
+    print ("Predicted digit: {}".format(prediction[0]))
+    plt.subplot(220+i)
+    plt.axis('off')
+    plt.title("Predicted digit: {}".format(prediction[0]))
+    plt.imshow(org_image, cmap=plt.get_cmap('gray'))
 
 plt.show()
 
